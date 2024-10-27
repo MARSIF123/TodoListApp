f8e589020ef75521fe5e232da9d6f30b5d66e8c1
MARSIF123 <maryamw561@gmail.com>
2024-10-27 09:27:27 -0400

    WIP: user context

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,18 @@ import "./App.css";
 import Home from "./pages/Home/Home";
 import Features from "./pages/Features/Features";
 import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import UserContextProvider from "./context/UserContextProvider";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/features" element={<Features />} />
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/features" element={<Features />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </UserContextProvider>
   );
 }
 
