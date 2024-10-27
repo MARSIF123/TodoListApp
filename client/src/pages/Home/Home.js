2c3b16fa153872a06064e0093d18cdec3962a5c7
MARSIF123 <maryamw561@gmail.com>
2024-10-27 08:51:25 -0400

    FEAT: add subtitle component

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,12 +2,16 @@ import React from "react";
 import styles from "./home.module.css";
 import PageLayout from "../PageLayout";
 import Title from "../../components/Title/Title";
+import Subtitle from "../../components/Subtitle/Subtitle";
 
 function Home() {
   return (
     <PageLayout>
       <div className={styles.main}>
         <Title>Empower Your Productivity</Title>
+        <Subtitle>
+          Make every day count with our task management solution.
+        </Subtitle>
       </div>
     </PageLayout>
   );
