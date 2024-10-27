de5c8df1f66362e928dbb224dad5f25e0f1550a9
MARSIF123 <maryamw561@gmail.com>
2024-10-27 07:40:42 -0400

    FEAT: added page layout

---
+++ client/src/pages/PageLayout.js
@@ -0,0 +1,19 @@
+import React from "react";
+import elipse from "../assets/Ellipse 2.png";
+import "../App.css";
+
+function PageLayout({ children }) {
+  return (
+    <div className="App">
+      <div className="max-width-wrapper">{children}</div>
+      <div className="elipse">
+        <img
+          src={elipse}
+          alt="an circle coloured with black and pink gradient"
+        ></img>
+      </div>
+    </div>
+  );
+}
+
+export default PageLayout;
