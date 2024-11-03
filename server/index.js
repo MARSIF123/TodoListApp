5853572490186a8bd25dd3c065f9b6de1406ba00
MARYAM <137287425+MARSIF123@users.noreply.github.com>
2024-11-03 16:59:12 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,8 +14,8 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors({
-  origin:["https://todo-list-app-rust-nine.vercel.app/"],
-  method:["GET","POST","PUT","DELETE"],
+  origin:["https://todo-list-app-rust-nine.vercel.app"],
+  methods:["GET","POST","PUT","DELETE"],
     credentials: true}));
 app.use("/api/tasks", taskRouter);
 app.use("/api/users", userRouter);
